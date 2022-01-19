f1fe6b1c236925e472a97b4ba5c6b7242d17fd45
enocPierre <enoc.pierre@mercadolivre.com>
2022-01-19 11:01:00 -0300

    search por filter

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -3,19 +3,20 @@ package repositorios
 import (
 	"api/src/modelos"
 	"database/sql"
+	"fmt"
 )
 
 // usuarios representa um repositorio de usuarios
-type Usuario struct {
+type Usuarios struct {
 	db *sql.DB
 }
 
 //novoRepositorioDeUsuarios cria um repositorio de usaurio
-func NovoRepositoriosDeUsuarios(db *sql.DB) *Usuario {
-	return &Usuario{db}
+func NovoRepositoriosDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
 }
 
-func (repositorio Usuario) Criar(usuario modelos.Usuario) (uint64, error) {
+func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 	statement, erro := repositorio.db.Prepare(
 		"insert into usuarios (nome, nick, email, senha) values(?, ?, ?, ?)",
 	)
@@ -36,4 +37,38 @@ func (repositorio Usuario) Criar(usuario modelos.Usuario) (uint64, error) {
 	}
 
 	return uint64(ultimoIDInserido), nil
+}
+
+//Buscar todos usuario que atendem um filtro de nome ou nick
+func (repositorio Usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error) {
+	nomeOuNick = fmt.Sprintf("%%%s%%", nomeOuNick)
+
+	linhas, erro := repositorio.db.Query(
+		"Select id, nome, nick, email, criadoEm from usuarios where nome LIKE ? or nick LIKE ?",
+		nomeOuNick, nomeOuNick,
+	)
+
+	if erro != nil {
+		return nil, erro	
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var usuario modelos.Usuario
+
+		if erro = linhas.Scan (
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro !=  nil {
+			return nil, erro
+		}
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
 }
--- api/src/router/controller/usuarioController.go
+++ api/src/router/controller/usuarioController.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +48,24 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando todos Usuárioio!"))
+	nomeOuNick := strings.ToLower(r.URL.Query().Get("usuario"))
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositoriosDeUsuarios(db)
+	usuarios, erro := repositorio.Buscar(nomeOuNick)
+
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, usuarios)
 }
 
 func BuscarUsauario(w http.ResponseWriter, r *http.Request) {
